0112968792341845c81680e059e8b86b6019bf1a
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-08-31 00:21:37 +0300

    commit tree c19685675117de1412c37cdbab2fcc0fb9e8d788

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'json'
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+   attr_accessor :url
+   def initialize(url)
+    @url = url
+   end
+   
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        programs = JSON.parse(self.get_response_body)
+        programs.collect do |program|
+          program
+        end
+    end
+end
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+puts get_requester.parse_json
